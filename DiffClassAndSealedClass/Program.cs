63b3e0a8a0c2064bc0be80d8204764058f8b035d
abdulkadirgul <abdulkadirgull1@gmail.com>
2023-11-04 22:07:43 +0300

    Add project files.

---
+++ DiffClassAndSealedClass/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<BenchmarkTest>();
+public class BenchmarkTest
+{
+    private readonly NoneSealedClass _noneSealedClass = new();
+    private readonly SealedClass _sealedClass = new();
+
+    [Benchmark]
+    public int NoneSealedClassBencmark()
+    {
+        return _noneSealedClass.Calculate(5, 7);
+    }
+
+    [Benchmark]
+    public int SealedClassBencmark()
+    {
+        return _sealedClass.Calculate(5, 7);
+    }
+}
+
+
+public sealed class SealedClass   //İnheritance alınamaz, Virtual Kullanılamaz  *DEVİRTUALİZATİON VARDIR*
+{
+    public int Calculate (int x,int y)
+    {
+        return x * y;
+    }
+}
+
+public class NoneSealedClass
+{
+    public virtual int Calculate(int x,int y)
+    {
+        return x * y;
+    }
+}
